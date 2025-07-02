844ddc61bf78a7f857a34107a3ae58df6359b17e
Carol <caroldazevedo52@gmail.com>
2025-07-02 13:30:18 -0300

    Create main.py

---
+++ Projeto/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+class UI:
+    def ui_visitante():
+        op = st.sidebar.selectbox("Menu", ["Entrar no Sistema", "Abrir Conta"])
+        if op == "Entrar no Sistema": LoginUI.main()
+        if op == "Abrir Conta": AbrirContaUI.main()
